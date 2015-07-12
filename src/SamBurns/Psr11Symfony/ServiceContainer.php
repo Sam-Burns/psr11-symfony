a23112c3073e6b5e15388ae60acd8a248a30b477
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-12 16:03:02 +0100

    Wrote some shit

--- src/SamBurns/Psr11Symfony/ServiceContainer.php
+++ src/SamBurns/Psr11Symfony/ServiceContainer.php
@@ -3,7 +3,9 @@ namespace SamBurns\Psr11Symfony;
 
 use Psr\Container\ContainerInterface;
 use SamBurns\Psr11Symfony\Exception\NotFoundException;
-use Symfony\Component\DependencyInjection\Container as SymfonyServiceContainer;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder as SymfonyServiceContainer;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 class ServiceContainer implements ContainerInterface
 {
@@ -23,7 +25,13 @@ class ServiceContainer implements ContainerInterface
      */
     public function addConfigFilesFromFolder($pathToFolder)
     {
+        $dirname = null;
+        $filename = null;
 
+        $loader = new PhpFileLoader($this->symfonyServiceContainer, new FileLocator($dirname));
+
+
+        $loader->load($filename);
     }
 
     /**
